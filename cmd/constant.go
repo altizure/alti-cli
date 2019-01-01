5a22b85c5b6f80cd9aec773a384af7dd0aa87e5f
Jacky Tang <jackytck@gmail.com>
2019-01-01 18:00:07 +0800

    :wrench: internal constants

---
+++ cmd/constant.go
@@ -0,0 +1,6 @@
+package cmd
+
+const defaultEndpoint = "https://api.altizure.com/graphql"
+const defaultAppKey = "Ah8bOakrkmSl2FA9OCbT8EnFOUrPwOOZ7HQxZm6"
+const defaultHostName1 = "api.altizure.com"
+const defaultHostName2 = "api.altizure.cn"
