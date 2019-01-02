1a35489fc5e673c00270e08c562ad716563dd043
Jacky Tang <jackytck@gmail.com>
2019-01-02 16:20:08 +0800

    :sparkles: load config

--- config/config.go
+++ config/config.go
@@ -16,7 +16,7 @@ type Config struct {
 	Token    string `yaml:"token"`
 }
 
-// Save saves the config in default paht.
+// Save saves the config in default path: '~/.altizure/config'.
 func (c Config) Save() error {
 	data, err := yaml.Marshal(c)
 	if err != nil {
@@ -30,6 +30,34 @@ func (c Config) Save() error {
 	if _, err2 := os.Stat(dir); os.IsNotExist(err2) {
 		os.Mkdir(dir, 0755)
 	}
-	err = ioutil.WriteFile(dir+"/credentials", data, 0644)
+	err = ioutil.WriteFile(dir+"/config", data, 0644)
 	return err
+}
+
+// DefaultConfig returns the default endpoint and api key.
+func DefaultConfig() Config {
+	return Config{
+		Endpoint: DefaultEndpoint,
+		Key:      DefaultAppKey,
+	}
+}
+
+// Load loads config from default path.
+func Load() (Config, error) {
+	dc := DefaultConfig()
+	home, err := homedir.Dir()
+	if err != nil {
+		return dc, err
+	}
+	config := fmt.Sprintf("%s/.altizure/config", home)
+	data, err := ioutil.ReadFile(config)
+	if err != nil {
+		return dc, err
+	}
+	var c Config
+	err = yaml.Unmarshal(data, &c)
+	if err != nil {
+		return dc, err
+	}
+	return c, nil
 }
