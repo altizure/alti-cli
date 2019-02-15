73936c2c708cd61be01a8aa5fa00648627217b13
Jacky Tang <jackytck@gmail.com>
2019-02-15 23:07:01 +0800

    :hammer: warn if removing non-empty project

--- cmd/project-remove.go
+++ cmd/project-remove.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/jackytck/alti-cli/gql"
 	"github.com/jackytck/alti-cli/types"
@@ -17,7 +18,24 @@ var projRemoveCmd = &cobra.Command{
 	Short: "Remove project by pid",
 	Long:  "Remove a project by its pid.",
 	Run: func(cmd *cobra.Command, args []string) {
-		p, err := gql.RemoveProject(id)
+		// check project info
+		p, err := gql.Project(id)
+		if err != nil {
+			fmt.Println("Project could not be removed! Error:", err)
+			return
+		}
+		if p.NumImage > 0 || p.GigaPixel > 0 {
+			var ans string
+			fmt.Printf("Warning: Project: %q is not empty. Are you sure? (Y/N): ", p.Name)
+			fmt.Scanln(&ans)
+			ans = strings.ToUpper(ans)
+			if ans != "Y" && ans != "YES" {
+				fmt.Println("Cancelled.")
+				return
+			}
+		}
+
+		_, err = gql.RemoveProject(id)
 		if err != nil {
 			fmt.Println("Project could not be removed! Error:", err)
 			return
