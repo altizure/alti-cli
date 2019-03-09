8f8d2b529b3b730ab9199dea88a976b6c7d38726
Jacky Tang <jackytck@gmail.com>
2019-03-10 01:01:04 +0800

    :art: support printing in table form after checking dir of images

--- cmd/check-imgage.go
+++ cmd/check-imgage.go
@@ -1,16 +1,20 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/jackytck/alti-cli/file"
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
 
 var dir string
 var skip string
 var verbose bool
+var printTable bool
 
 // checkImageCmd represents the checkImage command
 var checkImageCmd = &cobra.Command{
@@ -43,6 +47,9 @@ of all images of a given directory.`,
 			log.Printf("Working in %d thread(s)...", threads)
 		}
 
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"Filename", "Dimension", "GP", "Size (MB)", "Checksum"})
+
 		for r := range result {
 			if r.Error != nil {
 				log.Printf("Invalid image: %q, Reason: %v", r.Path, r.Error)
@@ -51,8 +58,19 @@ of all images of a given directory.`,
 
 			mb := file.BytesToMB(r.Filesize)
 			if verbose {
-				log.Printf("Path: %q, Filename: %q, Dimension: %dx%d, GP: %.2f, Size: %.2f MB, Checksum: %s\n",
+				log.Printf("Path: %q, Filename: %q, Dimension: %d x %d, GP: %.2f, Size: %.2f MB, Checksum: %s\n",
 					r.Path, r.Filename, r.Width, r.Height, r.GP, mb, r.SHA1)
+			}
+
+			if printTable {
+				r := []string{
+					fmt.Sprintf("%q", r.Filename),
+					fmt.Sprintf("%d x %d", r.Width, r.Height),
+					fmt.Sprintf("%.2f", r.GP),
+					fmt.Sprintf("%.2f", mb),
+					r.SHA1,
+				}
+				table.Append(r)
 			}
 
 			totalGP += r.GP
@@ -64,15 +82,21 @@ of all images of a given directory.`,
 		if err := <-errc; err != nil {
 			panic(err)
 		}
+		elapsed := time.Since(start)
 
 		if totalImg > 0 {
-			log.Printf("Found %d images, total %.2f GP", totalImg, totalGP)
+			log.Printf("Found %d images, total %.2f GP, %.2f MB", totalImg, totalGP, totalMB)
 		} else {
 			log.Println("No image is found!")
 		}
 
 		if verbose {
-			log.Println("Took", time.Since(start))
+			log.Println("Took", elapsed)
+		}
+
+		if printTable {
+			table.SetFooter([]string{fmt.Sprintf("%d image(s)", totalImg), "Total", fmt.Sprintf("%.2f GP", totalGP), fmt.Sprintf("%.2f MB", totalMB), fmt.Sprintf("Took %v", elapsed)})
+			table.Render()
 		}
 	},
 }
@@ -82,5 +106,6 @@ func init() {
 	checkImageCmd.Flags().StringVarP(&dir, "dir", "d", dir, "Directory path")
 	checkImageCmd.Flags().StringVarP(&skip, "skip", "s", skip, "Regular expression to skip paths")
 	checkImageCmd.Flags().BoolVarP(&verbose, "verbose", "v", verbose, "Display individual image info")
+	checkImageCmd.Flags().BoolVarP(&printTable, "table", "t", printTable, "Output all of the found images in table format")
 	checkImageCmd.MarkFlagRequired("dir")
 }
