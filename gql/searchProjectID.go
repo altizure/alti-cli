3ca4c447180a39f5daec18e04d1243454f85c3de
Jacky Tang <jackytck@gmail.com>
2019-03-19 15:58:54 +0800

    :sparkles: add gql to partial search pid

---
+++ gql/searchProjectID.go
@@ -0,0 +1,88 @@
+package gql
+
+import (
+	"context"
+	"net/url"
+	"time"
+
+	"github.com/jackytck/alti-cli/config"
+	"github.com/jackytck/alti-cli/errors"
+	"github.com/jackytck/alti-cli/types"
+	"github.com/machinebox/graphql"
+)
+
+// SearchProjectID returns the latest project id by the given partial id.
+func SearchProjectID(id string, myProj bool) (*types.Project, error) {
+	config := config.Load()
+	active := config.GetActive()
+	client := graphql.NewClient(active.Endpoint + "/graphql")
+
+	// make a request
+	req := graphql.NewRequest(`
+		query ($id: String, $limit:Int, $myProj: Boolean) {
+		  search {
+		    projectID(id: $id, limit: $limit, myProject: $myProj) {
+		      id
+					name
+					isImported
+					projectType
+					numImage
+					gigaPixel
+					taskState
+					date
+		    }
+		  }
+		}
+	`)
+	req.Var("id", id)
+	req.Var("limit", 1)
+	req.Var("myProj", myProj)
+	req.Header.Set("key", active.Key)
+	req.Header.Set("altitoken", active.Token)
+
+	// define a Context for the request
+	ctx := context.Background()
+
+	var res searchProjRes
+	if err := client.Run(ctx, req, &res); err != nil {
+		switch err.(type) {
+		case *url.Error:
+			return nil, errors.ErrOffline
+		default:
+			return nil, err
+		}
+	}
+
+	ps := res.Search.ProjectID
+	if len(ps) == 0 {
+		return nil, errors.ErrProjNotFound
+	}
+
+	p := types.Project{
+		ID:          ps[0].ID,
+		Name:        ps[0].Name,
+		IsImported:  ps[0].IsImported,
+		ProjectType: ps[0].ProjectType,
+		NumImage:    ps[0].NumImage,
+		GigaPixel:   ps[0].GigaPixel,
+		TaskState:   ps[0].TaskState,
+		Date:        ps[0].Date,
+	}
+
+	return &p, nil
+}
+
+type searchProjRes struct {
+	Search struct {
+		ProjectID []struct {
+			ID          string
+			Name        string
+			IsImported  bool
+			ProjectType string
+			NumImage    int
+			GigaPixel   float64
+			TaskState   string
+			Date        time.Time
+		}
+	}
+}
