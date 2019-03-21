646f6b8233ca4094a83534e091cc4b1bb50cf5ec
Jacky Tang <jackytck@gmail.com>
2019-03-21 18:39:00 +0800

    :sparkles: inspect my individual project by partial pid

---
+++ cmd/myproj-inspect.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jackytck/alti-cli/gql"
+	"github.com/jackytck/alti-cli/types"
+	"github.com/spf13/cobra"
+)
+
+// myprojInspectCmd represents the myprojInspect command
+var myprojInspectCmd = &cobra.Command{
+	Use:   "inspect",
+	Short: "Inspect my individual project by partial id.",
+	Long:  "Inspect my individual project details given by its id. ID could be a partial id.",
+	Run: func(cmd *cobra.Command, args []string) {
+		p, err := gql.SearchProjectID(id, true)
+		if err != nil {
+			fmt.Println("Project could not be found! Error:", err)
+			return
+		}
+		table := types.ProjectsToTable([]types.Project{*p}, os.Stdout)
+		table.Render()
+	},
+}
+
+func init() {
+	myprojCmd.AddCommand(myprojInspectCmd)
+	myprojInspectCmd.Flags().StringVarP(&id, "id", "p", id, "(Partial) Project id")
+	myprojInspectCmd.MarkFlagRequired("id")
+}
