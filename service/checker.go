9ca417dfbfdcef95ff6557e964200a57a1843892
Jacky Tang <jackytck@gmail.com>
2019-05-20 14:38:58 +0800

    :hammer: refactor checkers to be HOF

--- service/checker.go
+++ service/checker.go
@@ -10,58 +10,76 @@ import (
 	"github.com/jackytck/alti-cli/web"
 )
 
-// CheckAPIServer checks if API server is in normal mode.
-func CheckAPIServer(logger func(string, ...interface{})) error {
+// CheckFn represents a checker function.
+type CheckFn func(LogFn) error
+
+// LogFn represents a logger function. Same signature as log.Printf.
+type LogFn func(string, ...interface{})
+
+// Check checks all of the passed in checker functions.
+func Check(logger func(string, ...interface{}), cs ...CheckFn) error {
 	if logger == nil {
 		logger = log.Printf
 	}
-	mode := gql.ActiveSystemMode()
-	if mode != NormalMode {
-		logger("API server is in %q mode.\n", mode)
-		logger("Nothing could be uploaded at the moment!\n")
-		return errors.ErrOffline
+	for _, c := range cs {
+		err := c(logger)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
-// CheckUploadMethod checks if the supplied upload method is suppored.
-// kind is 'image', 'model' or 'meta'
-func CheckUploadMethod(kind string, method string, logger func(string, ...interface{})) error {
-	if logger == nil {
-		logger = log.Printf
-	}
-	if method == "" {
-		logger("No upload method is provided.")
-		return errors.ErrUploadMethodInvalid
-	}
-	supMethods := gql.SupportedCloud("", "", kind)
-	method = strings.ToLower(method)
-
-	// check direct upload
-	if method == DirectUploadMethod {
-		err := CheckDirectUpload(false, logger)
-		if err != nil {
-			logger("Supported upload methods are: %q!", supMethods)
-			return err
+// CheckAPIServer checks if API server is in normal mode.
+func CheckAPIServer() CheckFn {
+	return func(logger LogFn) error {
+		mode := gql.ActiveSystemMode()
+		if mode != NormalMode {
+			logger("API server is in %q mode.\n", mode)
+			logger("Nothing could be uploaded at the moment!\n")
+			return errors.ErrOffline
 		}
 		return nil
 	}
+}
 
-	// check s3 or oss
-	if sm := text.BestMatch(supMethods, method, ""); sm == "" {
-		logger("Upload method: %q is not supported!\n", method)
-		m := len(supMethods)
-		switch m {
-		case 0:
-			logger("No supported mehtod is found! You could only use 'direct' upload!")
-		case 1:
-			logger("Only %q upload is supported!", supMethods[0])
-		default:
-			logger("Supported upload methods are: %q!", supMethods)
+// CheckUploadMethod checks if the supplied upload method is suppored.
+// kind is 'image', 'model' or 'meta'
+func CheckUploadMethod(kind, method string) CheckFn {
+	return func(logger LogFn) error {
+		if method == "" {
+			logger("No upload method is provided.")
+			return errors.ErrUploadMethodInvalid
 		}
-		return errors.ErrUploadMethodInvalid
+		supMethods := gql.SupportedCloud("", "", kind)
+		method = strings.ToLower(method)
+
+		// check direct upload
+		if method == DirectUploadMethod {
+			err := CheckDirectUpload(false, logger)
+			if err != nil {
+				logger("Supported upload methods are: %q!", supMethods)
+				return err
+			}
+			return nil
+		}
+
+		// check s3 or oss
+		if sm := text.BestMatch(supMethods, method, ""); sm == "" {
+			logger("Upload method: %q is not supported!\n", method)
+			m := len(supMethods)
+			switch m {
+			case 0:
+				logger("No supported mehtod is found! You could only use 'direct' upload!")
+			case 1:
+				logger("Only %q upload is supported!", supMethods[0])
+			default:
+				logger("Supported upload methods are: %q!", supMethods)
+			}
+			return errors.ErrUploadMethodInvalid
+		}
+		return nil
 	}
-	return nil
 }
 
 // CheckDirectUpload checks if direct upload is supported.
@@ -69,7 +87,6 @@ func CheckDirectUpload(verbose bool, logger func(string, ...interface{})) error 
 	if logger == nil {
 		logger = log.Printf
 	}
-
 	logger("Checking direct upload...")
 	pu, _, err := web.PreferedLocalURL(verbose)
 	if err != nil {
@@ -78,4 +95,21 @@ func CheckDirectUpload(verbose bool, logger func(string, ...interface{})) error 
 	}
 	logger("Direct upload is supported over %q!\n", pu.Hostname())
 	return nil
+}
+
+// CheckPID checks if the pid of the right kind exists.
+// kind is "image" or "model".
+func CheckPID(kind, pid string) CheckFn {
+	return func(logger LogFn) error {
+		p, err := gql.SearchProjectID(pid, true)
+		if err != nil {
+			logger("Project could not be found! Error:", err)
+			return err
+		}
+		if kind == "image" && p.IsImported || kind == "model" && !p.IsImported {
+			logger("%q project could nont be found!", kind)
+			return errors.ErrProjNotFound
+		}
+		return nil
+	}
 }
