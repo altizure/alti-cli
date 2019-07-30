2b5ce70daf0d2f0f207b750190243b80e4b1ff3f
Jacky Tang <jackytck@gmail.com>
2019-07-30 15:57:26 +0800

    :sparkles: helper to download file from url

--- cloud/helper.go
+++ cloud/helper.go
@@ -1,6 +1,8 @@
 package cloud
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"os"
 
@@ -51,4 +53,34 @@ func PutFile(filepath string, url string) (*http.Response, error) {
 	}
 
 	return res, nil
+}
+
+// GetFile downloads a file from the given url and stores it in filepath.
+func GetFile(filepath string, url string) error {
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	// Writer the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
